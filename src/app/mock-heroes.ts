f1206acac6d1a4bbbaef0f323f17d6fe229ad486
Sea Dream <sea.dream0000@gmail.com>
2020-07-04 16:46:26 -0700

    feat: initial commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import {Hero} from "./hero";
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'AAA' },
+  { id: 12, name: 'BBB' },
+  { id: 13, name: 'CCC' },
+  { id: 14, name: 'DDD' },
+  { id: 15, name: 'EEE' },
+  { id: 16, name: 'FFF' },
+  { id: 17, name: 'GGG' },
+  { id: 18, name: 'HHH' },
+  { id: 19, name: 'III' },
+  { id: 20, name: 'JJJ' },
+];
